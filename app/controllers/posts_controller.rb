dad8833f93a9503e06fd95cea412b6789d70482e
Akhileshwar Reddy <akhil.vedas@outlook.com>
2025-03-21 05:55:25 +0000

    Implement APIs for post management along with tags

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,60 @@
+class PostsController < ApplicationController
+    before_action :authorized
+    before_action :set_post, only: [:show, :update, :destroy]
+    before_action :correct_user, only: [:update, :destroy]
+
+    def index
+        posts = Post.where(status: 'pubished')
+        render json: posts, include: [:user, :tags]
+    end
+
+    def show
+        render json: @post, include: [:user, :tags]
+    end
+
+    def create
+        post = current_user.posts.build(post_params)
+
+        if post.save
+            update_tags(post)
+            render json: post, include: :tags, status: :created
+        else
+            render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @post.update(post_params)
+            update_tags(@post)
+            render json: @post, include: :tags
+        else
+            render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @post.destroy
+        render json: { message: 'Post deleted successfully.'}
+    end
+
+    private
+
+    def set_post
+        @post = Post.find(params[:id])
+    end
+
+    def post_params
+        params.require(:post)
+            .permit(:title, :body, :status, :video, :status)
+    end
+
+    def update_tags(post)
+        post.tags = params[:post][:tag_names].map do |tag|
+            Tag.find_or_create_by(name: tag)
+        end
+    end
+
+    def correct_user
+        render json: { message: 'Not authorized' }, status: :unauthorized unless @post.user == current_user
+    end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
   validates :title, presence: true
-  validates :body, preseemce: true
+  validates :body, presence: true
   validates :status, inclusion: { in: %w(draft published archived) }
 end

b00ed0b248aeb82f71667f23b3e9abe91a7cfc18
Akhileshwar Reddy <akhil.vedas@outlook.com>
2025-03-21 16:05:25 +0000

    Implement comments APIs

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
+  has_many :comments, as: :commentable, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true
